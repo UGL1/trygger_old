79240d14a02b312b128c1d8a7d68f4f002d855e5
UGLi <ugli@mailo.com>
2023-03-24 08:28:44 +0100

    Put single_press and double_press actions in two separate threads

--- timer.py
+++ timer.py
@@ -4,46 +4,24 @@ from time import perf_counter_ns
 class Timer:
 
     def __init__(self, duration: int):
-        """        
-        :param duration: int in milliseconds
-        """
         self.duration = duration * 10 ** 6
         self.is_running = False
         self.last_time_reset = perf_counter_ns()
 
-    def start(self, duration=None) -> None:
-        """
-        Starts the timer
-        
-        :param duration: int in milliseconds
-        """
+    def start(self, duration: int = 0) -> None:
         self.last_time_reset = perf_counter_ns()
         self.is_running = True
-        if duration:
+        if duration != 0:
             self.duration = duration * 10 ** 6
 
-    def duration(self) -> float:
-        """
-        Returns timer's duration
-        """
-        return self.duration / 10 ** 6
+    def get_duration(self) -> float:
+        return self.duration // 10 ** 6
+
+    def set_duration(self, duration: float) -> None:
+        self.duration = duration * 10 ** 6
 
     def stop(self) -> None:
-        """
-        Stops the timer
-        """
         self.is_running = False
 
-    def has_expired(self, delay=False) -> bool:
-        """
-        If delay is not set, returns True or False depending on current time.
-        
-        If set, returns the same and a duration (positive if late, negative if early).
-       
-        :param delay: bool
-        """
-        if not delay:
-            return self.is_running and perf_counter_ns() - self.last_time_reset >= self.duration
-        else:
-            return self.is_running and perf_counter_ns() - self.last_time_reset >= self.duration, (
-                    self.is_running and perf_counter_ns() - self.last_time_reset - self.duration) / 10 ** 6
+    def has_expired(self) -> bool:
+        return not self.is_running or perf_counter_ns() - self.last_time_reset >= self.duration
--- trygger.py
+++ trygger.py
@@ -1,29 +1,33 @@
 from threading import Thread
 
 from kmhook import *
+
 from settings import *
 from timer import Timer
-import ctypes
-
 
 MessageBox = ctypes.windll.user32.MessageBoxW
-
+timer = dict()
+already_pressed_once = dict()
 
 
 class Trygger:
     def __init__(self):
-        self.actions = []
+        self.single_press_actions = []
+        self.double_press_actions = []
         self.action_threads = []
         self.go_on = False
 
-    def start(self,mainloop=False):
+    def start(self, mainloop=False):
         self.go_on = True
-        self.action_threads = [Thread(target=action) for action in self.actions]
+        self.action_threads = []
+        self.action_threads.append(Thread(target=self.single_press_actions_process))
+        self.action_threads.append(Thread(target=self.double_press_actions_process))
         for action_thread in self.action_threads:
             action_thread.start()
         if mainloop:
             MessageBox(0, 'Click OK to quit', 'Trygger', 0)
             self.stop()
+
     def stop(self):
         self.go_on = False
         for action_thread in self.action_threads:
@@ -31,35 +35,53 @@ class Trygger:
         self.action_threads = []
 
     def on_single_press(self, key: str | list | tuple):
-        def add_it(f):
-            def wrapper(*args, **kwargs):
-                while self.go_on:
-                    sleep(0.001)
-                    if is_pressed_once(key):
-                        f(*args, **kwargs)
+        if isinstance(key, list):
+            key = tuple(key)
 
-            self.actions.append(wrapper)
+        def add_it(f):
+            print(f, key)
+
+            def wrapper(*args, **kwargs):
+                if is_pressed_once(key):
+                    f(*args, **kwargs)
+
+            self.single_press_actions.append(wrapper)
             return wrapper
 
         return add_it
 
     def on_double_press(self, key: str | list | tuple):
-        def add_it(f):
-            def wrapper(*args, **kwargs):
-                timer = Timer(DOUBLE_PRESS_DURATION)
-                already_pressed_once = False
-                timer.start()
-                while self.go_on:
-                    sleep(0.001)
-                    if is_pressed_once(key):
-                        if not timer.has_expired():
-                            f(*args, **kwargs)
-                            already_pressed_once = False
-                        else:
-                            timer.start()
-                            already_pressed_once = False
+        if isinstance(key, list):
+            key = tuple(key)
 
-            self.actions.append(wrapper)
+        def add_it(f):  # fix that
+            timer[key] = Timer(DOUBLE_PRESS_DURATION)
+            already_pressed_once[key] = False
+            timer[key].start()
+
+            def wrapper(*args, **kwargs):
+
+                if is_pressed_once(key):
+                    if not timer[key].has_expired():
+                        f(*args, **kwargs)
+                        already_pressed_once[key] = False
+                    else:
+                        timer[key].start()
+                        already_pressed_once[key] = True
+
+            self.double_press_actions.append(wrapper)
             return wrapper
 
         return add_it
+
+    def single_press_actions_process(self):
+        while self.go_on:
+            sleep(0.001)
+            for action in self.single_press_actions:
+                action()
+
+    def double_press_actions_process(self):
+        while self.go_on:
+            sleep(0.001)
+            for action in self.double_press_actions:
+                action()
